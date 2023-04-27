7655e59b0b7baefac4ca687d0747eef964f77be9
sherwind <sherwin.doan@code88.asia>
2023-04-27 17:59:48 +0700

    Add websocket

---
+++ web-socket/src/main/java/com/example/websocket/model/Greeting.java
@@ -0,0 +1,14 @@
+package com.example.websocket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class Greeting {
+
+    private String content;
+
+}
---
+++ web-socket/src/main/java/com/example/websocket/model/HelloMessage.java
@@ -0,0 +1,15 @@
+package com.example.websocket.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+public class HelloMessage {
+
+    private String name;
+
+    private Long id;
+}
