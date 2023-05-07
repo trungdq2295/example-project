d9510e24757b21620ecbdc228a670fe8ccb86d5f
chickendje02 <chickendje02@gmail.com>
2023-05-07 14:55:28 +0700

    Init AWS IAM

---
+++ grpc-demo/grpc-client/src/main/java/com/example/grpc/grpc/client/model/Tessst.java
@@ -0,0 +1,7 @@
+package com.example.grpc.grpc.client.model;
+
+import org.springframework.boot.context.properties.ConfigurationProperties;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+@RestControllerAdvice
+public class Tessst {
+}
