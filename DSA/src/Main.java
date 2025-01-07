b6186616c4f56cc99e700cec569664aa009bebb8
trungdoan <trung.doan@mservice.com.vn>
2025-01-07 22:34:13 +0700

    DSA learning

--- DSA/src/Main.java
+++ DSA/src/Main.java
@@ -1,17 +1,29 @@
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
 // then press Enter. You can now see whitespace characters in your code.
 public class Main {
-    public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+  public static void main(String[] args) {
+//        // Press Alt+Enter with your caret at the highlighted text to see how
+//        // IntelliJ IDEA suggests fixing it.
+//        System.out.printf("Hello and welcome!");
+//
+//        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
+//        for (int i = 1; i <= 5; i++) {
+//
+//            // Press Shift+F9 to start debugging your code. We have set one breakpoint
+//            // for you, but you can always add more by pressing Ctrl+F8.
+//            System.out.println("i = " + i);
+//        }
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+    System.out.println(fib(3));
+  }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+  private static int fib(int n) {
+    int[] fbList = new int[n + 1];
+    fbList[0] = 0;
+    fbList[1] = 1;
+    for (int i = 2; i <= n; i++) {
+      fbList[i] = fbList[i - 1] + fbList[i-2];
     }
+    return fbList[n];
+  }
 }
